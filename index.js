5b2b5e983be9c504358f721013b57a3016fb5c1e
heinhtetyang <heinhtetyang@gmail.com>
2025-11-15 17:05:47 +0800

    Add app.use(cors())

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const Task = require("./models/Task");
 const Participation = require("./models/Participation");
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
